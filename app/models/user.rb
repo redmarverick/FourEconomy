8e6fab39ce810896bd846c42e0a8d239e98d9fa7
redmarverick <nyldrift@hotmail.com>
2023-09-13 20:40:17 -0300

    Create user.rb

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_and_belongs_to_many :groups
+
+  has_many :entities
+
+  validates :name, presence: true
+end
