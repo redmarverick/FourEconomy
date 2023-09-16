732714db07156d6d75028c0d8872b38dcc7dccaa
redmarverick <nyldrift@hotmail.com>
2023-09-15 23:29:20 -0300

    change user to group realtion

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_and_belongs_to_many :groups
+  has_many :groups, foreign_key: 'creator_id', dependent: :destroy
 
-  has_many :entities
+  has_many :entities, foreign_key: 'author_id', dependent: :destroy
 
   validates :name, presence: true
+
+  def admin?
+    self.admin == true
+  end
 end
