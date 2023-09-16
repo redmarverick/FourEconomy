8ae702801d3621f226f20ad8a1bb0cd84f847140
redmarverick <nyldrift@hotmail.com>
2023-09-15 23:29:05 -0300

    change group to user relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,8 @@
 class Group < ApplicationRecord
-  has_and_belongs_to_many :users
+  belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
   has_and_belongs_to_many :entities
 
   validates :name, presence: true
   validates :icon, presence: false
+  validates :creator_id, presence: true
 end
